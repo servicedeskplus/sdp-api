bce6b4b627468c7f76b6f65ebfa21e613455a7b2
servicedeskplus <integrations@servicedeskplus.com>
2025-05-23 16:58:40 +0530

    Latest Changes

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "SDP_OpenAPI_Collection.yml",
+    url: "OpenAPI_Collection.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
